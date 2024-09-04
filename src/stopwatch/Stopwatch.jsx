0996fc2a296e8ea12905fdd28cb000203dd51c38
shraddha potekar <shraddhapotekar762@gmail.com>
2024-09-04 18:48:04 +0530

    stopwatch commit

--- src/stopwatch/Stopwatch.jsx
+++ src/stopwatch/Stopwatch.jsx
@@ -29,14 +29,15 @@ const StopWatch=()=>{
 
 return(<div className={styles.mainDiv}>
 <h1>StopWatch</h1>
-<h3>Time:{formatTime(timer)}</h3>
-{!toggle?<button style={{paddingLeft:"0.5rem",paddingRight:"0.5rem",marginRight:"0.5rem"}} onClick={()=>{setToggle(true)}}>Start</button>
-:<button style={{paddingLeft:"0.5rem",paddingRight:"0.5rem",marginRight:"0.5rem"}} onClick={()=>{setToggle(false)}}>Stop</button>
+<h3>Time: {formatTime(timer)}</h3>
+{!toggle?<button style={{paddingLeft:"0.5rem",paddingRight:"0.5rem",borderRadius:"4px"}} onClick={()=>{setToggle(true)}}>Start</button>
+:<button style={{paddingLeft:"0.5rem",paddingRight:"0.5rem",borderRadius:"4px"}} onClick={()=>{setToggle(false)}}>Stop</button>
 }
-<button style={{paddingLeft:"0.5rem",paddingRight:"0.5rem"}} onClick={()=>{
+<button style={{paddingLeft:"0.5rem",paddingRight:"0.5rem",borderRadius:"4px"}} onClick={()=>{
     setToggle(false);
-    setTimer(0)
+    setTimer(0);
 }}>Reset</button>
+
 </div>)
 }
 export default StopWatch;
