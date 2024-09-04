1d0c741874dce888912017e7daa63105a9156f3a
shraddha potekar <shraddhapotekar762@gmail.com>
2024-09-04 19:05:32 +0530

    stopwatch commit

--- src/stopwatch/Stopwatch.jsx
+++ src/stopwatch/Stopwatch.jsx
@@ -2,41 +2,42 @@ import React, { useEffect } from "react";
 import styles from "./Stopwatch.module.css";
 import { useState } from "react";
 const StopWatch=()=>{
-    const[toggle,setToggle]=useState(false);
+    const[isRunning,setIsRunning]=useState(false);
     const[timer,setTimer]=useState(0);
 
-    useEffect(()=>{
-            let interval;
-            if(toggle)
-            {
-                 interval=setInterval(()=>{setTimer((prevTime)=>prevTime+1)},1000);
-            }
-            else{
-                clearInterval(interval);
-            }
-           return () => {
-            clearInterval(interval);
-        }  
-        
-       },[toggle]);
+    const resetBtn=()=>{
+        setIsRunning(false);
+        setTimer(0);
+    }
 
-
-       const formatTime = (seconds) => {
+    const formatTime = (seconds) => {
         const minutes = Math.floor(seconds / 60);
         const remainingSeconds = seconds % 60;
         return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
       };
 
+      useEffect(() => {
+        let intervalId;
+        if (isRunning) 
+            {
+            intervalId = setInterval(() => {
+                setTimer((prevTime) => prevTime + 1);
+            }, 1000);
+        } else if (!isRunning && intervalId) {
+            clearInterval(intervalId);
+        }
+
+        return () => clearInterval(intervalId);
+    }, [isRunning]);
+
 return(<div className={styles.mainDiv}>
 <h1>StopWatch</h1>
-<h3>Time: {formatTime(timer)}</h3>
-{!toggle?<button style={{paddingLeft:"0.5rem",paddingRight:"0.5rem",borderRadius:"4px"}} onClick={()=>{setToggle(true)}}>Start</button>
-:<button style={{paddingLeft:"0.5rem",paddingRight:"0.5rem",borderRadius:"4px"}} onClick={()=>{setToggle(false)}}>Stop</button>
+<p>Time: {formatTime(timer)}</p>
+{!isRunning?
+<button style={{paddingLeft:"0.5rem",paddingRight:"0.5rem",borderRadius:"4px"}} onClick={()=>{setIsRunning(true)}}>Start</button>
+:<button style={{paddingLeft:"0.5rem",paddingRight:"0.5rem",borderRadius:"4px"}} onClick={()=>{setIsRunning(false)}}>Stop</button>
 }
-<button style={{paddingLeft:"0.5rem",paddingRight:"0.5rem",borderRadius:"4px"}} onClick={()=>{
-    setToggle(false);
-    setTimer(0);
-}}>Reset</button>
+<button style={{paddingLeft:"0.5rem",paddingRight:"0.5rem",borderRadius:"4px"}} onClick={resetBtn}>Reset</button>
 
 </div>)
 }
